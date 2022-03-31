8c2b0b38e7479014b32cf367e770da088c5e04ff
Juuso <kortelaine@gmail.com>
2022-03-31 19:49:16 +0300

    small changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static("./public"));
 app.set("view engine","ejs");
 
 app.get("/", function(request,response){
-    var content = {
+    var content = { //using ejs to set heading and text
         heading: "WEATHER APP",
         text: "Search for a city:"
     };
